9a96b946eb212a0bd8cf1fc847d89ec1a9f006da
apsbains <apsi@udi.no>
2017-03-22 13:37:13 +0100

    changed with sonarlite

--- app-java/src/main/java/example/HelloWorld.java
+++ app-java/src/main/java/example/HelloWorld.java
@@ -1,20 +1,24 @@
 package example;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
 
+final static Logger logger = Logger.getLogger(HelloWorld.class);
+	
   public void coveredByUnitTest() {
-    System.out.println("coveredByUnitTest1");
-    System.out.println("coveredByUnitTest2");
+    logger.info("coveredByUnitTest1");
+    logger.info("coveredByUnitTest2");
   }
 
   public void coveredByIntegrationTest() {
-    System.out.println("coveredByIntegrationTest1");
-    System.out.println("coveredByIntegrationTest2");
-    System.out.println("coveredByIntegrationTest3");
+	  logger.info("coveredByIntegrationTest1");
+	  logger.info("coveredByIntegrationTest2");
+	  logger.info("coveredByIntegrationTest3");
   }
 
   public void notCovered() {
-    System.out.println("notCovered");
+	  logger.info("notCovered");
   }
 
 }
